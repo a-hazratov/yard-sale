6ed234f41b93886814664c1acda4bf97246b72b3
a-hazratov <artur.hazratov@gmail.com>
2021-06-25 21:24:06 +0800

    added chinese translation

--- main.js
+++ main.js
@@ -50,182 +50,182 @@ const saleItems = [
     {
        itemId: 4011,
        itemImage: ["img/air-pur-1.jpg", "img/air-pur-2.jpg", "img/air-pur-3.jpg"],
-       itemName: "Name-1",
-       itemPrice: 25,
-       itemDesc: "Some description here in Chinese"
+       itemName: "空气净化器",
+       itemPrice: 70,
+       itemDesc: "二手"
     },
 
     {
         itemId: 4012,
         itemImage: ["img/aircon-big-1.jpg", "img/aircon-big-2.jpg", "img/aircon-big-3.jpg", "img/aircon-big-4.jpg"],
-        itemName: "Name-2",
-        itemPrice: 25,
-        itemDesc: "Some description here in Chinese"
+        itemName: "空调",
+        itemPrice: 3000,
+        itemDesc: "二手，使用了 7 个月"
      },
 
      {
         itemId: 4013,
         itemImage: ["img/big-cabinet-1.jpg", "img/big-cabinet-2.jpg"],
-        itemName: "Name-3",
-        itemPrice: 25,
-        itemDesc: "Some description here in Chinese"
+        itemName: "储物柜",
+        itemPrice: 75,
+        itemDesc: "二手, 木制的"
      },
 
      {
         itemId: 4014,
         itemImage: ["img/blender-midea-1.jpg", "img/blender-midea-2.jpg"],
-        itemName: "Name-4",
-        itemPrice: 25,
-        itemDesc: "Some description here in Chinese"
+        itemName: "搅拌机 (美的)",
+        itemPrice: 180,
+        itemDesc: "新的"
      },
 
      {
         itemId: 4015,
         itemImage: ["img/blender-phil-1.jpg", "img/blender-phil-2.jpg"],
-        itemName: "Name-5",
-        itemPrice: 25,
-        itemDesc: "Some description here in Chinese"
+        itemName: "搅拌机 (飞利浦)",
+        itemPrice: 120,
+        itemDesc: "新的"
      },
 
      {
         itemId: 4016,
         itemImage: ["img/blow-heater-1.jpg", "img/blow-heater-2.jpg"],
-        itemName: "Name-6",
-        itemPrice: 25,
-        itemDesc: "Some description here in Chinese"
+        itemName: "暖风机",
+        itemPrice: 30,
+        itemDesc: "二手"
      },
      {
       itemId: 4017,
       itemImage: ["img/blue-mat-1.jpg", "img/blue-mat-2.jpg", "img/blue-mat-3.jpg"],
-      itemName: "Name-7",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "可折叠垫",
+      itemPrice: 80,
+      itemDesc: "二手"
    },
    {
       itemId: 4018,
       itemImage: ["img/book-cabinet-1.jpg", "img/book-cabinet-2.jpg", "img/book-cabinet-3.jpg"],
-      itemName: "Name-8",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "书柜",
+      itemPrice: 150,
+      itemDesc: "二手"
    },
    {
       itemId: 4019,
       itemImage: ["img/e-blanket-1.jpg", "img/e-blanket-2.jpg"],
-      itemName: "Name-9",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "电热毯",
+      itemPrice: 75,
+      itemDesc: "二手"
    },
    {
       itemId: 4020,
       itemImage: ["img/garden-chair-1.jpg", "img/garden-chair-2.jpg","img/garden-chair-3.jpg"],
-      itemName: "Name-10",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "折叠躺椅",
+      itemPrice: 100,
+      itemDesc: "二手, 有椅垫"
    },
    {
       itemId: 4021,
       itemImage: ["img/heater-1.jpg", "img/heater-2.jpg", "img/heater-3.jpg"],
-      itemName: "Name-11",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "加热器",
+      itemPrice: 65,
+      itemDesc: "二手, 很少使用"
    },
    {
       itemId: 4022,
       itemImage: ["img/scales-1.jpg", "img/scales-2.jpg"],
-      itemName: "Name-12",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "体质称",
+      itemPrice: 50,
+      itemDesc: "二手"
    },
    {
       itemId: 4023,
       itemImage: ["img/stand-lamp-1.jpg", "img/stand-lamp-2.jpg"],
-      itemName: "Name-13",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "落地灯",
+      itemPrice: 75,
+      itemDesc: "二手"
    },
    {
       itemId: 4024,
       itemImage: ["img/scooter-1.jpg", "img/scooter-2.jpg"],
-      itemName: "Name-14",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "电动车",
+      itemPrice: 1000,
+      itemDesc: "二手, 有头盔，手套"
    },
    {
       itemId: 4025,
       itemImage: ["img/super-desk-1.jpg", "img/super-desk-2.jpg"],
-      itemName: "Name-15",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "站立式工作桌",
+      itemPrice: 650,
+      itemDesc: "新的"
    },
    {
       itemId: 4026,
       itemImage: ["img/weights-1.jpg", "img/weights-2.jpg"],
-      itemName: "Name-16",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "锻炼哑铃",
+      itemPrice: 60,
+      itemDesc: "二手"
    },
    {
       itemId: 4027,
       itemImage: ["img/round-chair-1.jpg", "img/round-chair-2.jpg"],
-      itemName: "Name-17",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "椅子",
+      itemPrice: 40,
+      itemDesc: "二手, 没有椅垫 "
    },
    {
       itemId: 4028,
       itemImage: ["img/foam-1.jpg", "img/foam-2.jpg", "img/foam-3.jpg", "img/foam-4.jpg"],
-      itemName: "Name-18",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "海绵床垫",
+      itemPrice: 60,
+      itemDesc: "二手, "
    },
    {
       itemId: 4029,
       itemImage: ["img/couch-cover-1.jpg"],
-      itemName: "Name-19",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "沙发垫",
+      itemPrice: 55,
+      itemDesc: "二手"
    },
    {
       itemId: 4030,
       itemImage: ["img/oven-1.jpg"],
-      itemName: "Name-20",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "电烤箱",
+      itemPrice: 50,
+      itemDesc: "二手"
    },
    {
       itemId: 4031,
       itemImage: ["img/quilt-pink-1.jpg", "img/quilt-pink-2.jpg", "img/quilt-pink-3.jpg" ],
-      itemName: "Name-21",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "被子",
+      itemPrice: 150,
+      itemDesc: "二手, 有粉红色的床具"
    },
    {
       itemId: 4033,
       itemImage: ["img/room-cooler-1.jpg" ],
-      itemName: "Name-23",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "冷气机",
+      itemPrice: 100,
+      itemDesc: "卖光了"
    },
    {
       itemId: 4034,
       itemImage: ["img/curtains-beige-1.jpg" ],
-      itemName: "Name-24",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "窗帘",
+      itemPrice: 200,
+      itemDesc: "二手, 适合客厅"
    },
    {
       itemId: 4035,
       itemImage: ["img/curtains-brown-1.jpg" ],
-      itemName: "Name-25",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "窗帘",
+      itemPrice: 200,
+      itemDesc: "二手"
    },
    {
       itemId: 4036,
       itemImage: ["img/carpet-red-1.jpg" ],
-      itemName: "Name-26",
-      itemPrice: 25,
-      itemDesc: "Some description here in Chinese"
+      itemName: "地毯, 红色的",
+      itemPrice: 60,
+      itemDesc: "二手"
    }
 ];
 
@@ -246,8 +246,8 @@ function showItems (itemsArray) {
 
            <div class="post__bottom">
           <p>
-           Price: ${each.itemPrice} <br/>
-           Description: ${each.itemDesc}
+           价格:  &#165;${each.itemPrice} <br/>
+           细节:  ${each.itemDesc}.
           </p> 
           </div>
           <span class="rightArrow">&#10097;</span>
